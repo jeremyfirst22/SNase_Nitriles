e7375acd8984d44780eddcb00017178ca149d7c5
jeremyfirst22 <jeremy_first@utexas.edu>
2017-06-29 19:57:33 -0500

    Fixed minimage plotting script

--- Process_scripts/Process_minimage.py
+++ Process_scripts/Process_minimage.py
@@ -6,7 +6,7 @@ from os import sys
 from matplotlib.colors import LogNorm
 import matplotlib.lines as mlines 
 
-figCols=1
+figCols=2
 figRows=3
 
 if not os.path.isdir('figures') : 
@@ -17,34 +17,27 @@ fig.subplots_adjust(wspace=0)
 fig.text(0.5,0.04, "Time (ns)", ha='center', va='center') 
 fig.text(0.08,0.5, r"Distance to nearest image ($\AA$)", ha='center', va='center',rotation='vertical') 
 
+datafiles = glob.glob('*/minimage/mindist.xvg') 
+
 index=0
-for solvent in ['water','tert','sam'] : 
-    ax = axarr[index%figRows]
-    for state in ['folded','unfolded'] : 
-        datafile = '%s_%s/minimage/mindist.xvg'%(state,solvent) 
+for datafile in datafiles : 
+    print index, index/figCols, index%figRows
+    ax = axarr[index/figCols,index%figCols]
 
-        try : 
-            data = np.genfromtxt(datafile,skip_header=27) 
-        except IOError : 
-            print "No file found for %s %s"%(state,solvent)  
-            continue
-        if state == 'folded' : 
-            c='b' 
-        else :
-            c='g'
+    print datafile
+    try : 
+        data = np.genfromtxt(datafile,skip_header=27) 
+    except IOError : 
+        print "No file found for %s %s"%(state,solvent)  
+        continue
 
-        data[:,0] = data[:,0] / 1000 
-        data[:,1] = data[:,1] * 10 # nm -> Angstroms
+    data[:,0] = data[:,0] / 1000 
+    data[:,1] = data[:,1] * 10 # nm -> Angstroms
 
-        ax.scatter(data[:,0],data[:,1],s=0.1,color=c,label=state) 
-        ax.set_title('%s'%(solvent) ) 
-        ax.set_xlim(0,50) 
-        ax.set_ylim(10,50.0) 
+    ax.scatter(data[:,0],data[:,1],s=0.1,color='b') 
+    ax.set_title(datafile.split('/')[0]) 
+    ax.set_xlim(0,50) 
+    ax.set_ylim(10,50.0) 
 
     index +=1
-
-#blu_dot = mlines.Line2D([],[], linestyle='None',color='b', marker='o',label="folded") 
-#gre_dot = mlines.Line2D([],[], linestyle='None',color='g', marker='o',label="unfolded") 
-
-#plt.legend(handles=[gre_dot,blu_dot],numpoints=1,loc='center left', bbox_to_anchor=(1.0, 1.75))
 fig.savefig('figures/minimage.png',format='png') 
