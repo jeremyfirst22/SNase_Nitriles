0d063cac857c107f77a59a6d5cd5174868167433
jeremyfirst22 <jeremy_first@utexas.edu>
2019-05-10 09:55:54 -0500

    Changed figure size. Added rest of pKa data set from Garcia-Moreno to show all pKas do not agree with each other

--- Process_scripts/Process_pKa_vs_Peak.py
+++ Process_scripts/Process_pKa_vs_Peak.py
@@ -39,15 +39,23 @@ molecList = [
 
 rc_file('rc_files/paper.rc') 
 
-absMaxDict = {}  
-with open('Exp_data/abs_data.dat') as f : 
+absdata = 'Exp_data/abs_data2.dat'
+
+peakDict, fwhmDict = {}, {}
+with open('Exp_data/abs_data2.dat') as f :
+    for line in f :
+        if not line.startswith('#') :
+            #key = line.split()[0]
+            key, absMax, absStd, fwhm, fwhmStd = line.split()
+            peakDict[key] = [float(absMax),float(absStd)]
+            fwhmDict[key] = [float(fwhm),float(fwhmStd)]
+
+otherpKas = [] 
+with open('Exp_data/otherpKas.dat') as f : 
     for line in f : 
         if not line.startswith('#') : 
-            key = line.split()[0] 
-            absMax, FWHM, error = line.split()[1:4] 
-            absMax, FWHM, error = float(absMax), float(FWHM), float(error) 
-            absMaxDict[key] = [absMax,FWHM,error]
-
+            lys, glu = line.split() 
+            otherpKas.append([float(lys),float(glu)]) 
 
 rcFile='rc_files/paper.rc'
 rc_file(rcFile) 
@@ -59,7 +67,7 @@ left, right = 0.1, 0.75
 bottom, top = 0.06, 0.98
 hspace=0.35
 
-fig, axarr = plt.subplots(3,1,figsize=(3.25,5)) 
+fig, axarr = plt.subplots(3,1,figsize=(3.42,5)) 
 fig.subplots_adjust(left=left, bottom=bottom,right=right,top=top,hspace=hspace)
 fig.text((right-left)/2+left,top-(top-bottom)/3,           r"Nitrile frequency (cm$^{-1}$)", ha='center', va='bottom')
 fig.text((right-left)/2+left,bottom+(top-bottom)/3-0.03 ,           r"Nitrile frequency (cm$^{-1}$)", ha='center', va='bottom')
@@ -81,7 +89,9 @@ with open('Exp_data/pKas.dat') as f :
 
 ax1 = axarr[0]
 for molec in molecList : 
-    ax1.scatter(absMaxDict[molec][0],lysDict[molec],color=colorDict[molec]) 
+    peak,error = peakDict[molec]
+    ax1.scatter(peak,lysDict[molec],color=colorDict[molec]) 
+    ax1.errorbar(peak,lysDict[molec],xerr=error,color=colorDict[molec]) 
     ax1.axvline(2162.5,linestyle='--',color='k')
     ax1.axhline(10.4,linestyle='--',color='k')
     ax1.set_ylim([5,11.5]) 
@@ -89,18 +99,23 @@ for molec in molecList :
 
 ax2 = axarr[1]
 for molec in molecList : 
-    ax2.scatter(absMaxDict[molec][0],glnDict[molec],color=colorDict[molec],label=molec) 
+    peak,error = peakDict[molec]
+    ax2.scatter(peak,glnDict[molec],color=colorDict[molec],label=molec) 
+    ax2.errorbar(peak,glnDict[molec],xerr=error,color=colorDict[molec]) 
     ax2.axvline(2162.5,linestyle='--',color='k')
     ax2.axhline(4.5,linestyle='--',color='k')
     ax2.text(0.02,0.95,r"\textsf{B}",va='top',ha='left',transform=ax2.transAxes,fontsize=12)
 
-ax2 = axarr[2]
+ax3 = axarr[2]
 for molec in molecList : 
-    ax2.scatter(lysDict[molec],glnDict[molec],color=colorDict[molec]) 
-    ax2.axvline(10.4,linestyle='--',color='k')
-    ax2.axhline(4.5,linestyle='--',color='k')
-    ax2.text(0.02,0.95,r"\textsf{C}",va='top',ha='left',transform=ax2.transAxes,fontsize=12)
-    ax2.set_xlim(4.4,11)
+    ax3.scatter(lysDict[molec],glnDict[molec],color=colorDict[molec]) 
+    ax3.axvline(10.4,linestyle='--',color='k')
+    ax3.axhline(4.5,linestyle='--',color='k')
+    ax3.text(0.02,0.95,r"\textsf{C}",va='top',ha='left',transform=ax3.transAxes,fontsize=12)
+    ax3.set_xlim(4.4,11)
+for item in otherpKas : 
+    #print item
+    ax3.scatter(item[0],item[1],color='gray',s=4,marker='D') 
 
 
 
